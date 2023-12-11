537f24f0b5a7006e3f19edd26aaa32b2c5a37081
DESKTOP-JOJ5L1Q\82109 <c5ngdp@gmail.com>
2023-12-11 11:17:08 +0900

    섹션 2 - 회원 도메인 실행과 테스트

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // 의존하는게 두 가지 추상화에도 의존, 구체화에도 의존 즉 DIP를 위반
 
     @Override
     public void join(Member member) {
